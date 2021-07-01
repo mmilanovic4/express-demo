a72e36da4868f03000587a1f9610abc2588f38de
Miloš Milanović <mmilanovic016@gmail.com>
2021-07-01 12:43:23 +0200

    Move config to separate file

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from "express";
 import path from "path";
+import { config } from "./config";
 import { infoMiddleware } from "./middleware/infoMiddleware";
 import { apiRouter } from "./routes/apiRouter";
 import { homeRouter } from "./routes/homeRouter";
 
 const app = express();
-const port = 1234;
+const port = config?.port;
 
 // Configuration
 app.disable("x-powered-by");
 app?.use(express?.json());
+app?.use(express?.urlencoded({ extended: true }));
 app?.use("/static", express?.static(path?.resolve(__dirname, "static")));
 
 // Middleware
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  port: 5555,
+};
+
+export { config };
